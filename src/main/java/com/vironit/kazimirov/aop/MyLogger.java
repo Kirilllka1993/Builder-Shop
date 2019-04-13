df8b34992719dcc753a80568c3b5ad268d95192a
kirilllka1993 <kirilllka1993@gmail.com>
2019-04-13 12:26:43 +0300

    Commit all Project

--- src/main/java/com/vironit/kazimirov/aop/MyLogger.java
+++ src/main/java/com/vironit/kazimirov/aop/MyLogger.java
@@ -1,10 +1,9 @@
-package com.vironit.kazimirov.log;
+package com.vironit.kazimirov.aop;
 
 import org.apache.log4j.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
-import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
