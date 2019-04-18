dd2f7de5d65b268ec33d3acdaaa88be4a1cfed9e
kirilllka1993 <kirilllka1993@gmail.com>
2019-04-18 12:04:13 +0300

    Commit all Project

--- src/main/java/com/vironit/kazimirov/aop/AspectConfig.java
+++ src/main/java/com/vironit/kazimirov/aop/AspectConfig.java
@@ -1,4 +1,4 @@
-package com.vironit.kazimirov.config;
+package com.vironit.kazimirov.aop;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
